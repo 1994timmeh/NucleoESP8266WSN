eab903f5be22f0734b792f183e49ed42d33736f4
1994timmeh <timothy.hadwen@uqconnect.edu.au>
2015-04-15 13:07:18 +1000

    Working ultrasonic

--- WifiFreeRTOS/Ultrasonic.h
+++ WifiFreeRTOS/Ultrasonic.h
@@ -1,8 +1,10 @@
 #define SEC 0x7FFF00
+#include <stdint.h>
 
 void Ultrasonic_init();
 void Ultrasonic_start();
-int Ultrasonic_getdist();
+uint16_t Ultrasonic_getdist();
+uint16_t Ultrasonic_getwidth();
 void uDelay(unsigned long nCount);
 
 
