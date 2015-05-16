e9b9978cd040ed85e63241bb6f276d65d9aca669
1994timmeh <timmyhadwen@gmail.com>
2015-05-16 18:20:18 +1000

    Working DMA on Analog Pin A0. Sampling at 47.3kHz as per my logic analyser.

--- csse4011-project/main.c
+++ csse4011-project/main.c
@@ -24,12 +24,15 @@ ADC_HandleTypeDef AdcHandle2;
 ADC_ChannelConfTypeDef AdcChanConfig1;
 ADC_ChannelConfTypeDef AdcChanConfig2;
 TIM_HandleTypeDef TIM_Init;
+DMA_HandleTypeDef DMAHandle;
 
 int sampleNo = 0;
 
 //Temp storage for data as we fill the buffer
-uint32_t data1[1000];
-uint32_t data2[1000];
+uint16_t data1[256];
+uint32_t data2[256];
+
+uint8_t freq_out = 0, freq_out2 = 0;
 
 uint16_t value = 0;
 
@@ -39,9 +42,12 @@ void hardware_init();
 void adc_switch_channel_0( void );
 void adc_switch_channel_1( void );
 void tim2_irqhandler( void );
+void tim3_dma( void );
 void timer_interupt_init( void );
 void DMACompleteISR( void );
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle);
+
+void Error_Handler( void );
 
 
 /**
@@ -53,31 +59,29 @@ int main(void) {
 	unsigned int adc_value;
 
 	BRD_init();	//Initialise the NP2 board.
-	hardware_init();	//Initialise Hardware peripherals
-	//timer_interupt_init();
+	hardware_init();	//Initialise Hardware peripheral
+	timer_interupt_init();
 
-	while (1) {
-		Delay(0x1);
-		debug_printf("Bleh");
+	while (1){
 	}
 }
 
 
 void timer_interupt_init(){
-	unsigned short PrescalerValue;
+	uint32_t PrescalerValue;
+	TIM_MasterConfigTypeDef Tim3MasterConfigHandle;
 
 	/* Timer 4 clock enable */
-	__TIM4_CLK_ENABLE();
+	__TIM3_CLK_ENABLE();
 
 	/* Compute the prescaler value */
-	PrescalerValue = (uint16_t) ((SystemCoreClock /2)/480000) - 1; //Set this to be 480kHz
+	PrescalerValue = (uint16_t) ((SystemCoreClock/4)/515000) - 1;		//Set clock prescaler to 50kHz - SystemCoreClock is the system clock frequency.
 
-	/* Time base configuration */
-	TIM_Init.Instance = TIM4;						//Enable Timer 4
-	TIM_Init.Init.Period = 10;						//Set period count to be 1ms, so timer interrupt occurs every 1ms.
-	TIM_Init.Init.Prescaler = PrescalerValue;		//Set presale value
-	TIM_Init.Init.ClockDivision = 0;				//Set clock division
-	TIM_Init.Init.RepetitionCounter = 0;			// Set Reload Value
+	TIM_Init.Instance = TIM3;				//Enable Timer 2
+	TIM_Init.Init.Period = 10;			//Set period count to be 1ms, so timer interrupt occurs every 1ms.
+	TIM_Init.Init.Prescaler = PrescalerValue;	//Set presale value
+	TIM_Init.Init.ClockDivision = 0;			//Set clock division
+	TIM_Init.Init.RepetitionCounter = 0;	// Set Reload Value
 	TIM_Init.Init.CounterMode = TIM_COUNTERMODE_UP;	//Set timer to count up.
 
 	/* Initialise Timer */
@@ -85,20 +89,24 @@ void timer_interupt_init(){
 
 	/* Set priority of Timer 2 update Interrupt [0 (HIGH priority) to 15(LOW priority)] */
 	/* 	DO NOT SET INTERRUPT PRIORITY HIGHER THAN 3 */
-	HAL_NVIC_SetPriority(TIM4_IRQn, 3, 0);		//Set Main priority ot 10 and sub-priority ot 0.
+	HAL_NVIC_SetPriority(TIM3_IRQn, 3, 0);		//Set Main priority ot 10 and sub-priority ot 0.
+	NVIC_SetVector(TIM3_IRQn, (uint32_t)&tim2_irqhandler);
 
-	/* Enable timer update interrupt and interrupt vector for Timer  */
-	NVIC_SetVector(TIM4_IRQn, (uint32_t)&tim2_irqhandler);
-	NVIC_EnableIRQ(TIM4_IRQn);
+	Tim3MasterConfigHandle.MasterOutputTrigger	= TIM_TRGO_UPDATE;
+	Tim3MasterConfigHandle.MasterSlaveMode 		= TIM_MASTERSLAVEMODE_ENABLE;
+	HAL_TIMEx_MasterConfigSynchronization(&TIM_Init, &Tim3MasterConfigHandle);
+
+	NVIC_EnableIRQ(TIM3_IRQn);
 
 	/* Start Timer */
 	HAL_TIM_Base_Start_IT(&TIM_Init);
 }
 
 void tim2_irqhandler( void ){
-	//Clear Update Flag
 	__HAL_TIM_CLEAR_IT(&TIM_Init, TIM_IT_UPDATE);
-	HAL_ADC_Start(&AdcHandle1);
+	HAL_GPIO_WritePin(BRD_D1_GPIO_PORT, BRD_D1_PIN, 1);
+	Delay(1);
+	HAL_GPIO_WritePin(BRD_D1_GPIO_PORT, BRD_D1_PIN, 0);
 }
 
 /**
@@ -115,88 +123,119 @@ void hardware_init() {
 	/* Enable A0 GPIO Clock */
 	__BRD_A0_GPIO_CLK();
 	__BRD_A1_GPIO_CLK();
-	__BRD_D2_GPIO_CLK();
+	__BRD_D0_GPIO_CLK();
+	__BRD_A5_GPIO_CLK();
 
 	/* Enable ADC1 clock */
 	__ADC1_CLK_ENABLE();
 	__ADC2_CLK_ENABLE();
 
-	__DMA1_CLK_ENABLE();
+	__DMA2_CLK_ENABLE();
 
 	/* Configure A0, A1 as analog input */
-	GPIO_InitStructure.Pin = BRD_A0_PIN;			//Set A0 pin
-	GPIO_InitStructure.Mode = GPIO_MODE_ANALOG;		//Set to Analog input
-	GPIO_InitStructure.Pull = GPIO_NOPULL ;			//No Pull up resister
+	GPIO_InitStructure.Pin 		= BRD_A0_PIN;			//Set A0 pin
+	GPIO_InitStructure.Mode 	= GPIO_MODE_ANALOG;		//Set to Analog input
+	GPIO_InitStructure.Pull 	= GPIO_NOPULL ;			//No Pull up resister
 	HAL_GPIO_Init(BRD_A0_GPIO_PORT, &GPIO_InitStructure);	//Initialise AO
 
-	GPIO_InitStructure.Pin = BRD_A1_PIN;			//Set A0 pin
+	GPIO_InitStructure.Pin 		= BRD_A1_PIN;			//Set A0 pin
 	HAL_GPIO_Init(BRD_A1_GPIO_PORT, &GPIO_InitStructure);	//Initialise AO
 
-	//DMA Init
-	DMA_HandleTypeDef DMAHandle;
+	GPIO_InitStructure.Pin 		= BRD_D0_PIN;			//Set A0 pin
+	GPIO_InitStructure.Mode 	= GPIO_MODE_OUTPUT_PP;		//Set to Analog input
+	GPIO_InitStructure.Pull 	= GPIO_PULLDOWN ;			//No Pull up resister
+	HAL_GPIO_Init(BRD_D0_GPIO_PORT, &GPIO_InitStructure);	//Initialise AO
 
-	DMAHandle.Instance = DMA1_Stream0;
-	DMAHandle.Init.Channel = DMA_CHANNEL_0;
-	DMAHandle.Init.Direction = DMA_PERIPH_TO_MEMORY;
-	DMAHandle.Init.PeriphInc = DMA_PINC_DISABLE;
-	DMAHandle.Init.MemInc = DMA_MINC_ENABLE;
-	DMAHandle.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
-	DMAHandle.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
-	DMAHandle.Init.Mode = DMA_CIRCULAR;
-	DMAHandle.Init.Priority = DMA_PRIORITY_HIGH;
-	DMAHandle.Init.FIFOMode = DMA_FIFOMODE_ENABLE;
+	GPIO_InitStructure.Pin 		= BRD_D1_PIN;			//Set A0 pin
+	GPIO_InitStructure.Mode 	= GPIO_MODE_OUTPUT_PP;		//Set to Analog input
+	GPIO_InitStructure.Pull 	= GPIO_PULLDOWN ;			//No Pull up resister
+	HAL_GPIO_Init(BRD_D1_GPIO_PORT, &GPIO_InitStructure);	//Initialise AO
+
+	GPIO_InitStructure.Pin 		= BRD_A5_PIN;			//Set A0 pin
+	GPIO_InitStructure.Mode 	= GPIO_MODE_ANALOG;		//Set to Analog input
+	GPIO_InitStructure.Pull 	= GPIO_NOPULL ;			//No Pull up resister
+	HAL_GPIO_Init(BRD_A5_GPIO_PORT, &GPIO_InitStructure);	//Initialise AO
+
+	DMAHandle.Instance 					= DMA2_Stream0;
+	DMAHandle.Init.Channel				= DMA_CHANNEL_0;
+	DMAHandle.Init.Direction 			= DMA_PERIPH_TO_MEMORY;
+	DMAHandle.Init.PeriphInc 			= DMA_PINC_DISABLE;
+	DMAHandle.Init.MemInc 				= DMA_MINC_ENABLE;
+	DMAHandle.Init.PeriphDataAlignment 	= DMA_PDATAALIGN_HALFWORD;
+	DMAHandle.Init.MemDataAlignment 	= DMA_MDATAALIGN_HALFWORD;
+	DMAHandle.Init.Mode 				= DMA_CIRCULAR; //DMA_NORMAL
+	DMAHandle.Init.Priority 			= DMA_PRIORITY_LOW;
+	DMAHandle.Init.FIFOMode 			= DMA_FIFOMODE_ENABLE;
 	HAL_DMA_Init(&DMAHandle);
 
-	//NVIC_SetVector(DMA1_Stream0_IRQn, (uint32_t*)DMACompleteISR);
-	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_0);
-	HAL_NVIC_SetPriority(DMA1_Stream0_IRQn, 3, 3);
-	HAL_NVIC_EnableIRQ(DMA1_Stream0_IRQn);
-
 	/* ADC For A0 - Microphone 1 */
-	AdcHandle1.Instance = (ADC_TypeDef *)(ADC1_BASE);						//Use ADC1
-	AdcHandle1.Init.ClockPrescaler        = ADC_CLOCKPRESCALER_PCLK_DIV2;	//Set clock prescaler
-	AdcHandle1.Init.Resolution            = ADC_RESOLUTION12b;				//Set 12-bit data resolution
-	AdcHandle1.Init.ScanConvMode          = DISABLE;
-	AdcHandle1.Init.ContinuousConvMode    = ENABLE;
+	AdcHandle1.Instance = (ADC_TypeDef *)ADC1_BASE;
+	AdcHandle1.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV8;
+	AdcHandle1.Init.Resolution = ADC_RESOLUTION_12B;
+	AdcHandle1.Init.ScanConvMode = DISABLE;
+	AdcHandle1.Init.ContinuousConvMode = DISABLE;
 	AdcHandle1.Init.DiscontinuousConvMode = DISABLE;
-	AdcHandle1.Init.NbrOfDiscConversion   = 0;
-	AdcHandle1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;	//No Trigger
-	AdcHandle1.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T1_CC1;		//No Trigger
-	AdcHandle1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;				//Right align data
-	AdcHandle1.Init.NbrOfConversion       = 1;
+	AdcHandle1.Init.NbrOfDiscConversion = 1;
+	AdcHandle1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISINGFALLING;
+	AdcHandle1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T3_TRGO;
+	AdcHandle1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	AdcHandle1.Init.NbrOfConversion = 1;
 	AdcHandle1.Init.DMAContinuousRequests = ENABLE;
-	AdcHandle1.Init.EOCSelection          = DISABLE;
-	AdcHandle1.DMA_Handle = &DMAHandle;
+	AdcHandle1.Init.EOCSelection = DISABLE;
+
+	AdcHandle1.DMA_Handle 		= &DMAHandle;
+	DMAHandle.Parent 			= &AdcHandle1;
+
+	HAL_NVIC_SetPriority(DMA2_Stream0_IRQn, 10, 1);
+	HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+	NVIC_SetVector(DMA2_Stream0_IRQn, (uint16_t)&DMACompleteISR);
+
+	__HAL_LINKDMA(&AdcHandle1, DMA_Handle, DMAHandle);
 
 	if(HAL_ADC_Init(&AdcHandle1) != HAL_OK){
 		//Init failed
 		debug_printf("ERROR");
+		BRD_LEDOn();
 		for(;;);
 	}
 
+	HAL_ADC_Start_IT(&AdcHandle1);
+
 	/* Configure ADC Channel */
-	AdcChanConfig1.Channel = BRD_A0_ADC_CHAN;							//Use AO pin
+	AdcChanConfig1.Channel 		= BRD_A0_ADC_CHAN;	//Use AO pin
 	AdcChanConfig1.Rank         = 1;
 	AdcChanConfig1.SamplingTime = ADC_SAMPLETIME_3CYCLES;
 	AdcChanConfig1.Offset       = 0;
-	HAL_ADC_ConfigChannel(&AdcHandle1, &AdcChanConfig1);		//Initialise ADC channel
 
-	if(HAL_ADC_Start_DMA(&AdcHandle1, (uint32_t*)data1, 1000) != HAL_OK){
+	if( HAL_ADC_ConfigChannel(&AdcHandle1, &AdcChanConfig1) != HAL_OK){
+		debug_printf("ERROR");
+		BRD_LEDOn();
+		for(;;);
+	}
+
+	if(HAL_ADC_Start_DMA(&AdcHandle1, (uint32_t*)data1, 1) != HAL_OK){
 		//Init failed
+		BRD_LEDOn();
 		debug_printf("ERROR");
 		for(;;);
 	}
+
 }
 
-/**
- * This isnt being called for some reason :'(
- */
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* AdcHandle) {
+	//debug_printf("ADC: %d\n", data1[0]);
+}
+
+void HAL_ADC_ErrorCallback (ADC_HandleTypeDef * hadc){
+	debug_printf("ADC ERROR\n");
+}
+
 void DMACompleteISR( void ){
+	//Data transfer is complete!
 	HAL_DMA_IRQHandler(AdcHandle1.DMA_Handle);
-
-	BRD_LEDToggle();
-
-	//TODO Move sampled data into a new array for processing
+	HAL_GPIO_WritePin(BRD_D0_GPIO_PORT, BRD_D0_PIN, 1);
+	Delay(1);
+	HAL_GPIO_WritePin(BRD_D0_GPIO_PORT, BRD_D0_PIN, 0);
 }
 
 /**
@@ -239,3 +278,8 @@ void Delay(__IO unsigned long nCount) {
 //	GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;			//Pin latency
 //	HAL_GPIO_Init(BRD_D2_GPIO_PORT, &GPIO_InitStructure);	//Initialise Pin
 
+
+void Error_Handler(){
+	debug_printf("ERROR");
+	BRD_LEDOff();
+}
