6612a24f26e0cea1a53ee28c84ed63a3175a7570
michaelthoreau <michaelthoreau@me.com>
2015-06-02 21:46:59 +1000

    Revert "python WebAPI, nw.js, frontend, SQLite"
    This reverts commit 62a32cdf1e6bf37838d76bb439e937130b99d4a3.

--- Frontend/webAPI/csse4011_api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Node(models.Model):
-	Node_ID = models.SmallIntegerField();
-	latitude = models.CharField(max_length=21);
-	longitude = models.CharField(max_length=21);
-
-
--- Frontend/webAPI/csse4011_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-	url(r'^Nodes/$', views.Nodes, name='Nodes'),
-    url(r'^$', views.index, name='index'),
-]
